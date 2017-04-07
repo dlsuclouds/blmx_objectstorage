c238c8d22ad1da6a3d9ab372aa2760645c795028
dlsuclouds <dlsuclouds@gmail.com>
2017-04-07 19:36:09 +0800

    Trial update

--- src/main/java/net/tutorial/controllers/MainController.java
+++ src/main/java/net/tutorial/controllers/MainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 import net.tutorial.utilities.ObjectStorageService;
+import net.tutorial.utilities.TextToSpeechService;
 
-@WebServlet({ "home", "" })
+@WebServlet({ "/DoGetConvertText", "/DoGetFileUpload", "/DoPostConvertText", "/DoPostFileUpload", "home", "" })
 @MultipartConfig
 public class MainController extends HttpServlet {
 
@@ -23,28 +25,84 @@ public class MainController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		this.os = new ObjectStorageService();
-
-		List<String> documents = this.os.getDocumentList("Documents");
-		req.setAttribute("documents", documents);
-
-		dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
-		dispatcher.forward(req, resp);
+		try{
+			allServlets(req,resp);
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		final Part filePart = req.getPart("file");
-
-		this.os = new ObjectStorageService();
-
-		this.os.uploadFile("Documents", filePart);
-
-		List<String> documents = this.os.getDocumentList("Documents");
-		req.setAttribute("documents", documents);
-
-		dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
-		dispatcher.forward(req, resp);
+		try{
+			allServlets(req,resp);
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 	}
+
+	protected void allServlets((HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+
+		switch (request.getServletPath()) {
+
+			 case "/DoGetConvertText":
+			 	try{
+			 		dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
+					dispatcher.forward(req, resp);
+			 	} catch (Exception e){
+			 		e.printStackTrace();
+			 	}
+
+			 	break;
+
+			 case "/DoPostConvertText":
+				try{
+					TextToSpeechService service = new TextToSpeechService();
+					String text = req.getParameter("text");
+					service.getAudio(text, resp);
+			 	} catch (Exception e){
+			 		e.printStackTrace();
+			 	}
+
+			 	break;
+
+			 case "/DoGetFileUpload":
+			 	try{
+
+			 		this.os = new ObjectStorageService();
+
+					List<String> documents = this.os.getDocumentList("Documents");
+					req.setAttribute("documents", documents);
+
+					dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
+					dispatcher.forward(req, resp);
+
+			 	} catch (Exception e){
+			 		e.printStackTrace();
+			 	}
+
+			 	break;
+
+			 case "/DoPostFileUpload":
+			 	try{
+
+			 		final Part filePart = req.getPart("file");
+
+					this.os = new ObjectStorageService();
+
+					this.os.uploadFile("Documents", filePart);
+
+					List<String> documents = this.os.getDocumentList("Documents");
+					req.setAttribute("documents", documents);
+
+					dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/views/home.jsp");
+					dispatcher.forward(req, resp);
+
+			 	} catch (Exception e){
+			 		e.printStackTrace();
+			 	}
+
+			 	break;
 
 }

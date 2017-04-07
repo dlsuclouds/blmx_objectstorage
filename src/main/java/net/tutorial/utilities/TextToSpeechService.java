e12450601152c7126493cb0b70259f7f761f0d57
dlsuclouds <dlsuclouds@gmail.com>
2017-04-08 03:18:24 +0800

    Test update

--- src/main/java/net/tutorial/utilities/TextToSpeechService.java
+++ src/main/java/net/tutorial/utilities/TextToSpeechService.java
@@ -25,8 +25,8 @@ public class TextToSpeechService {
 	}
 
 	public void getAudio(String text, HttpServletResponse resp) throws IOException {
-		resp.setContentType("application/json");
-		resp.setHeader("Content-Disposition","attachment;filename=converted.wav");
+		resp.setContentType("text/xml");
+		resp.setHeader( "Content-Disposition", "filename=" + text );
 		
 		InputStream stream = service.synthesize(text, Voice.EN_ALLISON, AudioFormat.WAV).execute();
 		InputStream in = WaveUtils.reWriteWaveHeader(stream);
